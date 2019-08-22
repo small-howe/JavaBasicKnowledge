faf511ec92077668654c144178b9176b47924b3b
17702971861 <2678691035@qq.com>
2019-08-22 17:33:22 +0800

    init repo

---
+++ src/com/tangwh/fors/forDemo.java
@@ -0,0 +1,15 @@
+package com.tangwh.fors;
+
+public class forDemo {
+
+    public static void main(String[] args) {
+        /**
+         * for 循环(初始值;重点判断条件;变化量){
+         *
+         * }
+         */for (int i=0;i<5;i++){
+            System.out.println("跑了第"+i+"圈了");
+        }
+
+    }
+}
