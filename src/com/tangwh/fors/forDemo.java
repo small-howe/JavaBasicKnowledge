22be2385207770f2745761bc406500729724c70a
small-howe <2678691035@qq.com>
2019-08-22 18:04:05 +0800

    知识总结

--- src/com/tangwh/fors/forDemo.java
+++ src/com/tangwh/fors/forDemo.java
@@ -6,8 +6,10 @@ public class forDemo {
         /**
          * for 循环(初始值;重点判断条件;变化量){
          *
+         *  System.out.println("跑了第"+i+"圈了");
          * }
-         */for (int i=0;i<5;i++){
+         */
+        for (int i=0;i<5;i++){
             System.out.println("跑了第"+i+"圈了");
         }
 
