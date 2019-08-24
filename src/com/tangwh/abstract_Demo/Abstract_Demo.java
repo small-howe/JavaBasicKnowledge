c9ddc5f70e8354d8cf1263ecf11df7fb1f4b7a8f
small-howe <2678691035@qq.com>
2019-08-24 17:43:49 +0800

    '抽象类'

---
+++ src/com/tangwh/abstract_Demo/Abstract_Demo.java
@@ -0,0 +1,47 @@
+package com.tangwh.abstract_Demo;
+
+import sun.java2d.pipe.AAShapePipe;
+
+public abstract class Abstract_Demo {
+
+    // 抽象类  -----> 直接单继承 可以 ------> 抽象类
+    // 抽象类  -----> 直接单继承 可以 ------> 具体类
+    // 具体类  -----> 直接单继承 不可以 ----->抽象类(将父类的抽象方法具体化 或子类也变成抽象类)
+    // 抽象类 ------> 内部全部是具体方法-----> 可以
+
+    /**
+     *  1.可以含有构造放方法(包括 重载)
+     *
+     *  不能被实例化 创建
+     */
+    public Abstract_Demo(){
+
+
+    }
+
+    /**
+     *  1. 抽象类中可以含有属性(和正常类一样)
+     */
+    private static final String name="sads";
+
+    /**
+     *  2. 抽象类中可以含有一般的方法(和正常类一样)
+     *
+     */
+    public void show(){
+
+
+    }
+    /**
+     * 注意：3.抽象类中可以含有抽象方法的
+     */
+    public abstract  void showDemo();
+
+    /**
+     * 可以含有一般的成语块 也可以含有static 静态代码块
+     */
+    {
+
+    }
+
+}
