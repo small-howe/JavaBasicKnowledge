201870d9703a76a786826d1716283360eeda19cd
small-howe <2678691035@qq.com>
2019-08-24 10:26:09 +0800

    '运算符'

--- src/com/tangwh/fors/forDemo.java
+++ src/com/tangwh/fors/forDemo.java
@@ -1,10 +1,14 @@
 package com.tangwh.fors;
 
-import javax.sound.midi.SoundbankResource;
+
 
 public class forDemo {
-
     public static void main(String[] args) {
+        int[] list = new int[]{5,4,8,6,4,1,3,4,5,97,1,2};
+         bubbleSort_2(list);
+    }
+
+  public void showFor(){
         /**
          * for 循环(初始值;重点判断条件;变化量){
          *
@@ -27,5 +31,40 @@ public class forDemo {
         System.out.println(m);
         System.out.println(n);
         System.out.println(sum);
+  }
+
+
+
+    public  static void bubbleSort_2(int[] list){
+
+        int temp = 0; // 用来交换的临时数
+        boolean bChange = false; // 交换标志
+
+        // 要遍历的次数
+        for (int i = 0; i < list.length - 1; i++) {
+            bChange = false;
+            // 从后向前依次的比较相邻两个数的大小，遍历一次后，把数组中第i小的数放在第i个位置上
+            for (int j = list.length - 1; j > i; j--) {
+                // 比较相邻的元素，如果前面的数大于后面的数，则交换
+                if (list[j - 1] > list[j]) {
+                    temp = list[j - 1];
+                    list[j - 1] = list[j];
+                    list[j] = temp;
+                    bChange = true;
+                }
+
+            }
+
+            // 如果标志为false，说明本轮遍历没有交换，已经是有序数列，可以结束排序
+            if (false == bChange)
+                break;
+
+
+        }
+        for (int i1 : list) {
+            System.out.print(i1);
+        }
+
+
     }
 }
